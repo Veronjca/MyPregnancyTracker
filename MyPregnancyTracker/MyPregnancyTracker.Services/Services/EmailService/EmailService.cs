b96f1a803f747d153e5699121548886b1671b2cd
Veronica <veronicagoranova@gmail.com>
2022-10-14 18:35:26 +0300

    .

---
+++ MyPregnancyTracker/MyPregnancyTracker.Services/Services/EmailService/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Options;
+using MyPregnancyTracker.Data.Models;
+using MyPregnancyTracker.Services.Config;
+using MyPregnancyTracker.Services.EmailSender;
+using System.Reflection;
+using System.Text;
+using static MyPregnancyTracker.Services.Constants.Constants.Common;
+using static MyPregnancyTracker.Services.Constants.Constants.Email;
+
+namespace MyPregnancyTracker.Services.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEmailSender _emailSender;
+        private readonly MyPregnancyTrackerApplicationSettings _ngAppSettings;
+
+        public EmailService(
+            UserManager<ApplicationUser> userManager, 
+            IEmailSender emailSender,
+            IOptions<MyPregnancyTrackerApplicationSettings> ngAppSettings)
+        {
+            this._ngAppSettings = ngAppSettings.Value;
+            this._userManager = userManager;
+            this._emailSender = emailSender;
+        }
+        public async Task SendConfirmationEmailAsync(ApplicationUser user, string token)
+        {
+            string encodedToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+            string encodedUserId = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(user.Id.ToString()));
+            string htmlContent = GenerateHTMLContent(CONFIRMATION_EMAIL_HTML_TEMPLATE_FILE_NAME, encodedToken, encodedUserId);
+            await this._emailSender.SendEmailAsync(FROM, FROM_NAME, user.Email, SUBJECT, htmlContent);
+        }
+
+        private string GenerateHTMLContent(string fileName, string token, string userId)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + $"/EmailSender/Templates/{fileName}";
+            string htmlContentAsString = File.ReadAllText(path);
+
+            string uri = $"{_ngAppSettings.BaseUrl}/{_ngAppSettings.ConfirmEmailPageUrl}?emailToken={token}&userId={userId}"; 
+
+            string formattedHtmlContent = string.Format(htmlContentAsString, uri);
+            return formattedHtmlContent;
+        }
+    }
+}
---
+++ MyPregnancyTracker/MyPregnancyTracker.Services/Services/EmailService/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using MyPregnancyTracker.Data.Models;
+
+namespace MyPregnancyTracker.Services.Services.EmailService
+{
+    public interface IEmailService
+    {
+        /// <summary>
+        /// Sending confirmation email to the registered user, as asynchronous operation.
+        /// </summary>
+        /// <param name="user">The user itself.</param>
+        /// <param name="token">The token to use for the confirmation.</param>
+        Task SendConfirmationEmailAsync(ApplicationUser user, string token);
+    }
+}
